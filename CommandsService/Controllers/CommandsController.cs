b8588ff656d73d593ff50c57a62e3cf697b5b546
Tobias <tobi5116@edu.ucl.dk>
2021-11-11 18:07:03 +0100

    Added ComandsController that allows us to create commands to platforms

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET: api/<CommandsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+
+            if (!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+            var commands = _repository.GetCommandsForPlatform(platformId);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        // GET api/<CommandsController>/5
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId} / {commandId}");
+            if (!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+            var command = _repository.GetCommand(platformId, commandId);
+
+            if (command == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+
+        // POST api/<CommandsController>
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto commandDto)
+        {
+            Console.WriteLine($"--> Hit CreateCommandForPlatform: {platformId}");
+
+            if (!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = _mapper.Map<Command>(commandDto);
+
+            _repository.CreateCommand(platformId, command);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(command);
+
+
+            return CreatedAtRoute(nameof(GetCommandForPlatform),
+                new { platformId = platformId, commandId = commandReadDto.Id}, commandReadDto);
+        }
+
+        // PUT api/<CommandsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CommandsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
